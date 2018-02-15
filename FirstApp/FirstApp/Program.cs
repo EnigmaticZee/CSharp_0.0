0ae04b3fc0147f6797ee23dc2d93735d047ae300
EnigmaticZee <zaidshakilz@gmail.com>
2018-02-15 22:06:15 +1100

    Update

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,22 @@
+﻿//All the basic utilities and primitive types are here
+using System; 
+//Work with lists, collections and so on
+using System.Collections.Generic;
+//Works with Data
+using System.Linq;
+//Works with text and coding
+using System.Text;
+//Used to build multi-threaded applications
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
